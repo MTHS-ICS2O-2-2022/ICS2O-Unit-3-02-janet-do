750377f05a01e68b0e9cbb86f92f02e5ef070be7
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-05 21:36:36 +0000

    fixed

--- go_app/main.go
+++ go_app/main.go
@@ -8,29 +8,29 @@ package main
 import "fmt"
 
 func main() {
-    // This function finds the volume of a pyramid
+	// This function finds the volume of a pyramid
 
-    var length float64
-    var width float64
-    var height float64
-    var volume float64
+	var length float64
+	var width float64
+	var height float64
+	var volume float64
 
-    // input
-    fmt.Println("This program finds the volume of a pyramid.")
-    fmt.Println()
-    fmt.Print("Enter the length (in cm): ")
-    fmt.Scanln(&length)
-    fmt.Print("Enter the width (in cm): ")
-    fmt.Scanln(&width)
-    fmt.Print("Enter the height (in cm): ")
-    fmt.Scanln(&height)
-    fmt.Println()
+	// input
+	fmt.Println("This program finds the volume of a pyramid.")
+	fmt.Println()
+	fmt.Print("Enter the length (in cm): ")
+	fmt.Scanln(&length)
+	fmt.Print("Enter the width (in cm): ")
+	fmt.Scanln(&width)
+	fmt.Print("Enter the height (in cm): ")
+	fmt.Scanln(&height)
+	fmt.Println()
 
-    // process
-    volume = (length * width * height) / 3.0
+	// process
+	volume = (length * width * height) / 3.0
 
-    // output
-    fmt.Printf("The volume of the pyramid is: %.2f cm³\n", volume)
-    fmt.Println()
-    fmt.Println("Done.")
+	// output
+	fmt.Printf("The volume of the pyramid is: %.2f cm³\n", volume)
+	fmt.Println()
+	fmt.Println("Done.")
 }
